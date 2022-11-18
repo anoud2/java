e4197a74405ecbe5068e5296b5b700802d5daf83
anoud2 <anoud.j.alsaud@gmail.com>
2022-11-18 09:03:49 +0300

    Create Welcome.java

---
+++ welcome/Welcome.java
@@ -0,0 +1,11 @@
+package welcome;
+
+// this code was made to revise basics of java code
+public class Welcome {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to java");
+        System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+
+    }
+}
