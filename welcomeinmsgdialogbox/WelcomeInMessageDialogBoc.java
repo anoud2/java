b786fed0db5294fd3aafbf5a141b13077a7033b0
anoud2 <anoud.j.alsaud@gmail.com>
2022-11-18 08:56:01 +0300

    Create WelcomeInMessageDialogBoc.java

---
+++ welcomeinmsgdialogbox/WelcomeInMessageDialogBoc.java
@@ -0,0 +1,14 @@
+
+/*
+This application program display Welcome to Java!
+in a message dialog box.
+ */
+package welcomeinmsgdialogbox;
+import javax.swing.JOptionPane;
+public class WelcomeInMessageDialogBoc {
+    public static void main(String[] args) {
+        //display welcome to java! in msg dialog box
+        JOptionPane.showMessageDialog(null, "Welcome to Java!");
+    }
+    
+}
